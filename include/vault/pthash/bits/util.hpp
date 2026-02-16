8e1fff5d51b23acccdfeac8a81efad2ae6a039fa
Chris Hayden <christopher.maurice.hayden@gmail.com>
2026-02-16 15:17:13 +0000

    Remove unused headers.

--- include/vault/pthash/bits/util.hpp
+++ include/vault/pthash/bits/util.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bit>
 #include <cassert>
 #include <cstdint>
 
@@ -9,49 +10,58 @@
 
 namespace bits::util {
 
-/*
-    Good reference for built-in functions:
-    http://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
-*/
+  /*
+      Good reference for built-in functions:
+      http://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+  */
 
-/* return the position of the most significant bit (msb) */
-static inline uint64_t msb(uint32_t x) {
-    assert(x > 0);                 // if x is 0, the result is undefined
-    return 31 - __builtin_clz(x);  // count leading zeros (clz)
-}
-static inline uint64_t msbll(uint64_t x) {
-    assert(x > 0);                   // if x is 0, the result is undefined
-    return 63 - __builtin_clzll(x);  // count leading zeros (clz)
-}
-static inline bool msbll(uint64_t x, uint64_t& ret) {
+  /* return the position of the most significant bit (msb) */
+  static inline uint64_t msb(uint32_t x) {
+    assert(x > 0);                // if x is 0, the result is undefined
+    return 31 - __builtin_clz(x); // count leading zeros (clz)
+  }
+
+  static inline uint64_t msbll(uint64_t x) {
+    assert(x > 0);                  // if x is 0, the result is undefined
+    return 63 - __builtin_clzll(x); // count leading zeros (clz)
+  }
+
+  static inline bool msbll(uint64_t x, uint64_t& ret) {
     if (x) {
-        ret = 63 - __builtin_clzll(x);
-        return true;
+      ret = 63 - __builtin_clzll(x);
+      return true;
     }
     return false;
-}
+  }
 
-static inline uint64_t ceil_log2_uint32(uint32_t x) { return (x > 1) ? msb(x - 1) + 1 : 0; }
-static inline uint64_t ceil_log2_uint64(uint64_t x) { return (x > 1) ? msbll(x - 1) + 1 : 0; }
+  static inline uint64_t ceil_log2_uint32(uint32_t x) {
+    return (x > 1) ? msb(x - 1) + 1 : 0;
+  }
 
-/* return the position of the least significant bit (lsb) */
-static inline uint64_t lsb(uint32_t x) {
-    assert(x > 0);            // if x is 0, the result is undefined
-    return __builtin_ctz(x);  // count trailing zeros (ctz)
-}
-static inline uint64_t lsbll(uint64_t x) {
-    assert(x > 0);              // if x is 0, the result is undefined
-    return __builtin_ctzll(x);  // count trailing zeros (ctz)
-}
-static inline bool lsbll(uint64_t x, uint64_t& ret) {
+  static inline uint64_t ceil_log2_uint64(uint64_t x) {
+    return (x > 1) ? msbll(x - 1) + 1 : 0;
+  }
+
+  /* return the position of the least significant bit (lsb) */
+  static inline uint64_t lsb(uint32_t x) {
+    assert(x > 0);           // if x is 0, the result is undefined
+    return __builtin_ctz(x); // count trailing zeros (ctz)
+  }
+
+  static inline uint64_t lsbll(uint64_t x) {
+    assert(x > 0);             // if x is 0, the result is undefined
+    return __builtin_ctzll(x); // count trailing zeros (ctz)
+  }
+
+  static inline bool lsbll(uint64_t x, uint64_t& ret) {
     if (x) {
-        ret = __builtin_ctzll(x);
-        return true;
+      ret = __builtin_ctzll(x);
+      return true;
     }
     return false;
-}
+  }
 
-static inline uint64_t popcount(uint64_t x) {
+  static inline uint64_t popcount(uint64_t x) {
 #ifdef __SSE4_2__
     return static_cast<uint64_t>(_mm_popcnt_u64(x));
 #elif __cplusplus >= 202002L
@@ -59,27 +69,27 @@ static inline uint64_t popcount(uint64_t x) {
 #else
     return static_cast<uint64_t>(__builtin_popcountll(x));
 #endif
-}
+  }
 
-/*
-    Return the position of the i-th 1 in the word,
-    assuming i < popcount(word).
-*/
-static inline uint64_t select_in_word(uint64_t word, uint64_t i) {
+  /*
+      Return the position of the i-th 1 in the word,
+      assuming i < popcount(word).
+  */
+  static inline uint64_t select_in_word(uint64_t word, uint64_t i) {
     assert(i < popcount(word));
 #ifndef __BMI2__
     // Modified from: Bit Twiddling Hacks
     // https://graphics.stanford.edu/~seander/bithacks.html#SelectPosFromMSBRank
-    unsigned int s;       // Output: Resulting position of bit with rank r [1-64]
-    uint64_t a, b, c, d;  // Intermediate temporaries for bit count.
-    unsigned int t;       // Bit count temporary.
-    uint64_t k = popcount(word) - i;
-    a = word - ((word >> 1) & ~0UL / 3);
-    b = (a & ~0UL / 5) + ((a >> 2) & ~0UL / 5);
-    c = (b + (b >> 4)) & ~0UL / 0x11;
-    d = (c + (c >> 8)) & ~0UL / 0x101;
-    t = (d >> 32) + (d >> 48);
-    s = 64;
+    unsigned int s;          // Output: Resulting position of bit with rank r [1-64]
+    uint64_t     a, b, c, d; // Intermediate temporaries for bit count.
+    unsigned int t;          // Bit count temporary.
+    uint64_t     k = popcount(word) - i;
+    a              = word - ((word >> 1) & ~0UL / 3);
+    b              = (a & ~0UL / 5) + ((a >> 2) & ~0UL / 5);
+    c              = (b + (b >> 4)) & ~0UL / 0x11;
+    d              = (c + (c >> 8)) & ~0UL / 0x101;
+    t              = (d >> 32) + (d >> 48);
+    s              = 64;
     s -= ((t - k) & 256) >> 3;
     k -= (t & ((t - k) >> 8));
     t = (d >> (s - 16)) & 0xff;
@@ -99,10 +109,10 @@ static inline uint64_t select_in_word(uint64_t word, uint64_t i) {
     return s - 1;
 #else
     uint64_t k = 1ULL << i;
-    asm("pdep %[word], %[mask], %[word]" : [ word ] "+r"(word) : [ mask ] "r"(k));
-    asm("tzcnt %[bit], %[index]" : [ index ] "=r"(k) : [ bit ] "g"(word) : "cc");
+    asm("pdep %[word], %[mask], %[word]" : [word] "+r"(word) : [mask] "r"(k));
+    asm("tzcnt %[bit], %[index]" : [index] "=r"(k) : [bit] "g"(word) : "cc");
     return k;
 #endif
-}
+  }
 
-}  // namespace bits::util
+} // namespace bits::util
